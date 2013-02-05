f618d2244b91606f4884db53e8ca63a11fa31491
Jeff <3xamine@gmail.com>
2013-02-05 15:29:12 -0700

    Added tabulated-list-mode derivation example.

--- collab-mode.el
+++ collab-mode.el
@@ -3,4 +3,19 @@
   (interactive)
   (pop-to-buffer (get-buffer-create "users")))
 
-(define-derived-mode users-mode)
+(define-derived-mode collab-users-mode tabulated-list-mode "Users Mode"
+  "Major mode for managing connections with users"
+  (setq tabulated-list-format [("Users" 18 t)])
+  (setq tabulated-list-padding 2)
+  (tabulated-list-init-header))
+
+(defun print-current-line-id ()
+  (interactive)
+  (message (concat "current line ID is: " (tabulated-list-get-id))))
+
+(defun collab-list-users ()
+  (interactive)
+  (pop-to-buffer "*Users*" nil)
+  (collab-users-mode)
+  (setq tabulated-list-entries (list (list "2" ["a" "b" "c" "d"])))
+  (tabulated-list-print t))
