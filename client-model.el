b6789be64f3799aaf642dd48f995a57a727f6c31
Andrew Cobb <andrew.cobb@gmail.com>
2013-02-19 13:39:30 -0700

    factored out a more opaque network object

--- client-model.el
+++ client-model.el
@@ -23,29 +23,47 @@ so it doesn't rebroadcast itself into an infinite loop")
 (defun collab-mode-cm-after-change-hook (start end previous-length)
  "handler for hook that the buffer just changed"
  (when (/= previous-length 0)
-  (collab-mode-network-post-delete start (+ start previous-length)))
+  (collab-mode-network-post-delete
+   collab-mode-cm-network-connection
+   start (+ start previous-length)))
  (when (/= start end)
-  (collab-mode-network-post-insert start (buffer-substring-no-properties start end))))
+  (collab-mode-network-post-insert
+   collab-mode-cm-network-connection
+   start (buffer-substring-no-properties start end))))
 
 (defvar collab-mode-cm-other-buffer nil)
+(defvar collab-mode-cm-network-connection nil)
 
 (defun collab-mode-cm-init (&optional other-buffer)
+ "Initialization for the client model
+TBD: how many times is this called, and in what contexts"
  (set (make-local-variable 'collab-mode-cm-applying-changes) nil)
  (set (make-local-variable 'collab-mode-cm-other-buffer)
   (or other-buffer (get-buffer-create "*mirror*")))
+ (set (make-local-variable 'collab-mode-cm-network-connection)
+  (collab-mode-network-connect "localhost" 1234))
  (unless other-buffer
-  (let ((first-buffer (current-buffer))
-        (string (buffer-string)))
-   (with-current-buffer collab-mode-cm-other-buffer
-    (delete-region (point-min) (point-max))
-    (insert string)
-    (collab-mode-cm-init first-buffer))))
+   (collab-mode-network-init-remote-document collab-mode-cm-network-connection (buffer-string)))
  (add-hook 'after-change-functions #'collab-mode-cm-after-change-hook nil t))
 
 ;; STUB FUNCTIONS
-(defun collab-mode-network-post-delete (start end)
- (with-current-buffer collab-mode-cm-other-buffer
+(defun collab-mode-network-post-delete (network-obj start end)
+ "pretends to send delete over the network"
+ (with-current-buffer (cadr network-obj)
   (collab-mode-cm-delete start end)))
-(defun collab-mode-network-post-insert (start string)
- (with-current-buffer collab-mode-cm-other-buffer
+
+(defun collab-mode-network-post-insert (network-obj start string)
+ "pretends to send insert over the network"
+ (with-current-buffer (cadr network-obj)
   (collab-mode-cm-insert string start)))
+
+(defun collab-mode-network-init-remote-document (network-obj string)
+ "pretends to send the entire document across the network to start editing"
+ (with-current-buffer (cadr network-obj)
+  (delete-region (point-min) (point-max))
+  (insert string)
+  (collab-mode-cm-init (caddr network-obj))))
+
+(defun collab-mode-network-connect (host port)
+ "pretends to connect to collabserver located at host:port"
+ `(opaque-network-object ,collab-mode-cm-other-buffer ,(current-buffer)))
