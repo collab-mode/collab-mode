9d4721c8bf3c11724aea7979755c75243d349c73
Joel Hough <joel@instructure.com>
2013-02-21 15:55:35 -0700

    added some network buffering

--- network.el
+++ network.el
@@ -4,9 +4,15 @@
  (setq collab-server-process (open-network-stream "collab-server" "*collab-server*" "ec2.alcobb.com" 10068))
  (set-process-filter collab-server-process #'collab-network-receive-from-server))
 
+(setq collab-server-input-buffer "")
+
 (defun collab-network-receive-from-server (process data)
   ; TODO: Handle fragments
-  (infinote-execute (read data)))
+  (setq collab-server-input-buffer (concat collab-server-input-buffer data))
+  (let ((messages (split-string data "[\000]")))
+    (when (> (length messages) 1)
+      (infinote-execute (read (car messages)))
+      (setq collab-server-input-buffer (mapconcat 'identity (cdr messages) "\000")))))
 
 (defun collab-network-send-to-server (text)
   (process-send-string collab-server-process (concat "<message>" text "\000")))
