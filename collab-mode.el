42d9197b62c1cf9cba15376df85c407a7a5b3257
rjeffj <3xamine@gmail.com>
2013-02-21 12:10:40 -0700

    List entries populated from function.

--- collab-mode.el
+++ collab-mode.el
@@ -13,6 +13,11 @@
   (interactive)
   (pop-to-buffer "*Users*" nil)
   (collab-users-mode)
-  (setq tabulated-list-entries '(("1" [("● user1" . (action (lambda (arg) (message "blah"))))])
-				 ("2" [("○ user2" . (face (:foreground "red" :underline t) ))])))
+  (setq tabulated-list-entries (collab-get-entries))
   (tabulated-list-print t))
+
+(defun collab-get-entries ()
+  "Returns the tabulated-list-entries argument for listing users. It gets the list of
+users, checks which are connected, and returns the appropriate list of entries."
+  (list '("1" [("● user1" . (action (lambda (arg) (message "blah"))))])
+    '("2" [("○ user2" . (face (:foreground "red" :underline t) ))])))
