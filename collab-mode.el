42214d3d8b5135de18b0a74c7d1604072b8a38be
rjeffj <3xamine@gmail.com>
2013-02-26 16:46:21 -0700

    Connect/disconnect mechanism working with "Jeff" user test."

--- collab-mode.el
+++ collab-mode.el
@@ -1,15 +1,20 @@
 (define-derived-mode collab-users-mode tabulated-list-mode "Users Mode"
-  "Major mode for managing connections with users"
-  (setq tabulated-list-format [("Users" 18 t)])
+  "Major mode for managing connections with users."
+  ;; Width doesn't matter because there's only one column (so just set it to zero).
+  (setq tabulated-list-format [("Users" 0 t)])
+  ;; Adds two spaces to the left of the list.
   (setq tabulated-list-padding 2)
   (tabulated-list-init-header))
 
 (defun collab-list-users ()
+  "This is the function that is called by the user to bring up the users list."
   (interactive)
   (pop-to-buffer "*Users*" nil)
   (collab-users-mode)
   (setq tabulated-list-entries (collab-get-entries))
   (tabulated-list-print t))
+
+
 
 (defun collab-get-entries ()
   "Returns the tabulated-list-entries argument for listing users. It gets the list of
@@ -18,29 +23,47 @@ users, checks which are connected, and returns the appropriate list of entries."
   (let ()
     (setq entries)
     (dolist (user (collab-users))
-      (setq entries
-	    (append entries (list (list nil (vector (cons (collab-user-text user)
-							  `(face (:foreground ,(collab-user-color user) :underline t)))))))))
-    (print entries)))
+      (setq entries (append entries (list (list user (vector (cons (collab-user-text user)
+							  `(face (:foreground ,(collab-user-color user) :underline t)
+								 action collab-connect-user button "blah"))))))))
+    (print entries))) ;; @debug (print)
 
 (defun collab-user-text (user)
-  "Returns user entry label with appropriate face and connection glyph."
+  "Returns user entry label with appropriate connection glyph."
   (if (collab-user-connected user)
       (concat "● " user)
     (concat "○ " user)))
 
+(defun collab-connect-user (user)
+  (if (string= "Jeff" (tabulated-list-get-id user))
+      (if collab-jeff-connected
+	  (setq collab-jeff-connected nil)
+	(setq collab-jeff-connected 'true)))
+  (setq tabulated-list-entries (collab-get-entries))
+  (tabulated-list-print)
+  (print (tabulated-list-get-id user))) ;; @debug (print)
+
+(setq collab-jeff-connected ())
+
 (defun collab-user-connected (user)
-  "Returns whether USER is connected."
+  "Returns whether USER is connected.
+This is a temporary function. Actual function will be provided by the client."
   (if (string= "Jeff" user)
-      'true
+      collab-jeff-connected
     nil))
 
+(defun test-jeff-connected ()
+(interactive)
+(print (collab-user-connected "Jeff")))
+
 (defun collab-users ()
-  "Returns a list of all available users."
+  "Returns a list of all available users.
+This is a temporary function. Actual function will be provided by the client."
   (list "Andrew" "Joel" "Jeff"))
 
 (defun collab-user-color (user)
-  "Returns USER's color."
+  "Returns USER's color.
+This is a temporary function. Actual function will be provided by the client."
   (if (string= "Jeff" user)
       "red"
     "blue"))
