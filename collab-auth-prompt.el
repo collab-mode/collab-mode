17eeb961c0c5c6e20f89e72ab758ed672346785b
Jeff J <jjenkins@greypointcorp.com>
2013-03-20 18:11:00 -0600

    Added auth code and example.

--- collab-auth-prompt.el
+++ collab-auth-prompt.el
@@ -1,4 +1,12 @@
 (require 'password-cache)
 
-(password-read "Password ")
-(read-from-minibuffer "Username: ")
+(defun collab-login ()
+  "Prompts for a username and password. Passes username as car and
+password as cdr to COLLAB-MODE-CM-XMPP-LOGIN."
+  (interactive)
+  (collab-mode-cm-xmpp-login
+   (cons (read-from-minibuffer "Username: ")
+	 (password-read "Password: "))))
+
+(defun collab-mode-cm-xmpp-login (info)
+  (message  "username: %s, password: %s" (car info) (cdr info)))
